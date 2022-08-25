5722a2b9e3d23ea1e90a111c776bf4e8cf8a8b05
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-25 16:40:29 +0400

    доделал таймер

--- src/module/timer.js
+++ src/module/timer.js
@@ -1,6 +1,7 @@
 const timer = (dedLine) => {
   console.log('Модуль one++');
 
+  const timerDays = document.getElementById('timer-days');
   const timerHours = document.getElementById('timer-hours');
   const timerMinutes = document.getElementById('timer-minutes');
   const timerSeconds = document.getElementById('timer-seconds');
@@ -10,11 +11,13 @@ const timer = (dedLine) => {
     let dateNow = new Date().getTime();
     let timeRemaining = (dateStop - dateNow) / 1000;
 
-    let hours = Math.floor(timeRemaining / 60 / 60);
+    let days = Math.floor(timeRemaining / 60 / 60 / 24);
+    let hours = Math.floor((timeRemaining / 60 / 60) % 24);
     let minutes = Math.floor((timeRemaining / 60) % 60);
     let seconds = Math.floor(timeRemaining % 60);
 
     return {
+      days,
       hours,
       minutes,
       seconds,
@@ -22,25 +25,46 @@ const timer = (dedLine) => {
     };
   };
 
+  const getZero = () => {
+    let getTime = getTimeRemaining();
+
+    if (timerDays.textContent.length === 1) {
+      timerDays.textContent = '0' + timerDays.textContent;
+    }    
+
+    if (timerHours.textContent.length === 1) {
+      timerHours.textContent = '0' + timerHours.textContent;
+    }
+
+    if (timerMinutes.textContent.length === 1) {
+      timerMinutes.textContent = '0' + timerMinutes.textContent;
+    }
+
+    if (timerSeconds.textContent.length === 1) {
+      timerSeconds.textContent = '0' + timerSeconds.textContent;
+    }
+  };
+
   const updateClock = () => {
     let getTime = getTimeRemaining();
 
-    timerHours.textContent = getTime.hours;
-    timerMinutes.textContent = getTime.minutes;
-    timerSeconds.textContent = getTime.seconds;
+    // console.log(typeof getTime.hours);
 
     if (getTime.timeRemaining > 0) {
-      setTimeout(updateClock, 1000);
+      timerDays.textContent = getTime.days;
+      timerHours.textContent = getTime.hours;
+      timerMinutes.textContent = getTime.minutes;
+      timerSeconds.textContent = getTime.seconds;
     } else {
       timerHours.textContent = '00';
       timerMinutes.textContent = '00';
       timerSeconds.textContent = '00';
     }
+
+    getZero();
   };
 
-  updateClock();
-
-  //   setInterval(countTimer, 1000, '5 september 2022');
+  setInterval(updateClock, 1000);
 };
 
 export default timer;
