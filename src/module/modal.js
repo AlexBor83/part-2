c21b0caa533a8ae4dffacc8964f8eddc50f1fb4e
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-27 14:51:13 +0400

    Добавил анимацию

--- src/module/modal.js
+++ src/module/modal.js
@@ -1,20 +1,41 @@
 const modal = () => {
     const popupBtns = document.querySelectorAll('.popup-btn');
     const modal = document.querySelector('.popup');
+    const modalContent = modal.querySelector('.popup-content');
     const modalClose = modal.querySelector('.popup-close');
+    let count = 0;
+    let animaite;
+    
+    const animaiteModal = () => {
+        count++;
+        console.log(count);
+
+        animaite = requestAnimationFrame (animaiteModal);
+
+        if (count < 20) {
+            modalContent.style.left = count*3 + '%'; 
+        } else {
+            cancelAnimationFrame(animaite);
+        }        
+    };
 
     popupBtns.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            modal.style.display = 'block';   
+        btn.addEventListener('click', () => {            
+            modal.style.display = 'block';
+            
+            if(window.innerWidth > 768) {
+                animaiteModal();
+            } else {
+                modalContent.style.left = '50%';
+                modalContent.style.transform = 'translateX(-50%)';
+            }            
         });
     });
 
     modalClose.addEventListener('click', () => {
         modal.style.display = 'none';
-    });
-
-    console.log(modal);
+        count = 0;
+    });    
 };
-
 
 export default modal;
