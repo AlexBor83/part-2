aac17ea09481645a1751c08db054ab75aef140af
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-29 21:25:19 +0400

    добавил услажненку

--- src/module/menu.js
+++ src/module/menu.js
@@ -1,34 +1,36 @@
 const menu = () => {
+  const body = document.querySelector('body');
   const menuBtn = document.querySelector('.menu');
   const scrollBtn = document.querySelector('main>a');
 
   const menu = document.querySelector('menu');
-  const closeBtn = menu.querySelector('.close-btn');
-  const menuItems = menu.querySelectorAll('ul>li>a');
+  // const closeBtn = menu.querySelector('.close-btn');
+  // const menuItems = menu.querySelectorAll('ul>li>a');
 
   const handelMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
-  menuBtn.addEventListener('click', handelMenu);
-
-  menu.addEventListener('click', (e) => {
-    
-    if (e.target.matches('li a')) {
-      e.preventDefault();
+  body.addEventListener('click', (e) => {
+    if (e.target.closest('.menu')) {
       handelMenu();
-      const item = e.target.closest('a');
-      const itemId = item.getAttribute('href').substring(1);
-      
-      document.getElementById(itemId).scrollIntoView({
-        block: 'start',
-        behavior: 'smooth',
-      });
     }
 
     if (e.target.classList.contains('close-btn')) {
       e.preventDefault();
       handelMenu();
+    }
+
+    if (e.target.matches('li a')) {
+      e.preventDefault();
+      handelMenu();
+      const item = e.target.closest('a');
+      const itemId = item.getAttribute('href').substring(1);
+
+      document.getElementById(itemId).scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+      });
     }
   });
 
