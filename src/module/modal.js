a535d2e6dc8b1dddca1a048b503b4636e70fd61e
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-29 18:11:17 +0400

    Делегирование модального окна и табов

--- src/module/modal.js
+++ src/module/modal.js
@@ -1,42 +1,41 @@
 const modal = () => {
-    const popupBtns = document.querySelectorAll('.popup-btn');
-    const modal = document.querySelector('.popup');
-    const modalContent = modal.querySelector('.popup-content');
-    const modalClose = modal.querySelector('.popup-close');
-    let count = 0;
-    let animaite;
+  const popupBtns = document.querySelectorAll('.popup-btn');
+  const modal = document.querySelector('.popup');
+  const modalContent = modal.querySelector('.popup-content');
+  let count = 0;
+  let animaite;
 
-    modalContent.style.transform = 'translateX(-50%)'; 
-    
-    const animaiteModal = () => {
-        count++;
-       
-        animaite = requestAnimationFrame (animaiteModal);
+  modalContent.style.transform = 'translateX(-50%)';
 
-        if (count <= 10) {
-            modalContent.style.left = count*5 + '%'; 
-        } else {
-            cancelAnimationFrame(animaite);
-        }        
-    };
+  const animaiteModal = () => {
+    count++;
+    animaite = requestAnimationFrame(animaiteModal);
 
-    popupBtns.forEach((btn) => {
-        btn.addEventListener('click', () => {            
-            modal.style.display = 'block';
-            
-            if(window.innerWidth > 768) {
-                animaiteModal();
-            } else {
-                modalContent.style.left = '50%';
-                // modalContent.style.transform = 'translateX(-50%)';
-            }            
-        });
+    if (count <= 10) {
+      modalContent.style.left = count * 5 + '%';
+    } else {
+      cancelAnimationFrame(animaite);
+    }
+  };
+
+  popupBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      modal.style.display = 'block';
+
+      if (window.innerWidth > 768) {
+        animaiteModal();
+      } else {
+        modalContent.style.left = '50%';        
+      }
     });
+  });
 
-    modalClose.addEventListener('click', () => {
-        modal.style.display = 'none';
-        count = 0;
-    });    
+  modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+      modal.style.display = 'none';
+      count = 0;      
+    }
+  });
 };
 
 export default modal;
