f029bebdbe037c19f9b7c2b5177148c56670c21a
AlexBor83 <rabota_for_alex@mail.ru>
2022-09-05 20:17:04 +0400

    поправил меню

--- src/module/menu.js
+++ src/module/menu.js
@@ -4,32 +4,21 @@ const menu = () => {
   const scrollBtn = document.querySelector('main>a');
 
   const menu = document.querySelector('menu');
-  
+
   const handelMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
   body.addEventListener('click', (e) => {
     if (e.target.closest('.menu')) {
-      handelMenu();      
-    }
-
-    if (e.target.classList.contains('close-btn')) {
+      handelMenu();
+    } else if (
+      e.target.classList.contains('close-btn') ||
+      !e.target.closest('.menu')
+    ) {
       e.preventDefault();
       handelMenu();
     }
-
-    if (menu.classList.contains('active-menu')) {
-      
-      if (!e.target.classList.contains('active-menu')) {
-      console.log('пустое место');        
-      }
-    }
-
-    
-
-
-
 
     if (e.target.matches('li a')) {
       e.preventDefault();
