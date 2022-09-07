8fbac2bac3ace7fb9fa7520de67c11710e15e7c0
AlexBor83 <rabota_for_alex@mail.ru>
2022-09-07 17:33:50 +0400

    Поправил валидацию

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import timer from './module/timer';
 import menu from './module/menu';
 import modal from './module/modal';
-import form from './module/form';
 import calculator from './module/calculator';
 import tabs from './module/tabs';
 import slider from './module/slider';
 import sendForm from './module/sendForm';
 
-timer('5 september 2022');
+timer('21 september 2022');
 menu();
 modal();
-form()
 calculator(100);
 tabs();
 slider(
--- src/module/sendForm.js
+++ src/module/sendForm.js
@@ -7,15 +7,57 @@ const sendForm = ({ formId, someElem = [] }) => {
   const succsessText = 'Данные отправлены';
 
   const validate = (list) => {
-    let succsess = true;
+    const inputText = form.querySelector('[type=text]');
+    const inputTel = form.querySelector('[type=tel]');
+    const inputEmail = form.querySelector('[type=email]');
 
-    return succsess;
-  };
+    let isError = false;
+
+    if (!/[^а-яА-Я\s]/g.test(inputText.value) && inputText.value.trim() != '') {
+      inputText.style.border = 'none';
+    } else {
+      isError = true;
+      alert('Введите Ваше Имя на русском');
+      inputText.style.border = '2px solid red';
+    }
+
+    if (!/[^\d\-\()]/g.test(inputTel.value) && inputTel.value != '') {
+      inputTel.style.border = 'none';
+    } else {
+      isError = true;
+      alert('В ведите номер телефона');
+      inputTel.style.border = '2px solid red';
+    }
+    
+    if (
+      /[\w\@\-\_\.\!\~\*\']/g.test(inputEmail.value) &&
+      inputEmail.value != ''
+    ) {
+      inputEmail.style.border = 'none';
+    } else {
+      isError = true;
+      alert('В ведите Ваш E-mail');
+      inputEmail.style.border = '2px solid red';
+    }
+
+    if(form.querySelector('.mess')) {
+      const inputMess = form.querySelector('.mess');
+
+      if (!/[^а-яА-Я]/g.test(inputMess.value) && inputMess.value != '') {
+        inputMess.style.border = 'none';
+      } else {
+        isError = true;
+        alert('Введите Ваше сообщение');
+        inputMess.style.border = '2px solid red';
+      }
+    }    
+    return isError;
+  };  
 
   const sendData = (data) => {
     // или fetch('/server.php' - для gitPages
     // fetch('https://jsonplaceholder.typicode.com/posts' - локально
-    return fetch('https://jsonplaceholder.typicode.com/posts', { 
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
@@ -30,6 +72,7 @@ const sendForm = ({ formId, someElem = [] }) => {
     const formBody = {};
 
     statusBlock.textContent = loadText;
+    statusBlock.style.color = 'violet';
     form.append(statusBlock);
 
     formData.forEach((value, key) => {
@@ -46,31 +89,45 @@ const sendForm = ({ formId, someElem = [] }) => {
       }
     });
 
-    if (validate(formElements)) {
+    if (!validate(formElements)) {
       sendData(formBody)
-      .then((data) => {
-        statusBlock.textContent = succsessText;
-        
-        formElements.forEach((input) => {
-          input.value = '';
-        });        
-      })
-      .catch((error) => {
-        statusBlock.textContent = errorText;        
-      });     
+        .then((data) => {
+          statusBlock.textContent = succsessText;
+
+          const cleanStatusBlock = () => {
+            statusBlock.textContent = '';
+          };
+          
+          const closeModal = () => {
+            const modal =document.querySelector('.popup');
+            modal.style.display = 'none';
+          };
+          
+          setTimeout(cleanStatusBlock, 2000);
+          setTimeout(closeModal, 4000);
+
+          formElements.forEach((input) => {
+            input.value = '';
+          });
+        })
+        .catch((error) => {
+          statusBlock.textContent = errorText;
+        });
+    } else {
+      statusBlock.textContent = '';
+      alert('Данные не валидны');
     }
   };
 
   try {
-
-    if(!form) {
-        throw new Error('Верните форму')
+    if (!form) {
+      throw new Error('Верните форму');
     }
 
     form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        submitForm();    
-      });
+      e.preventDefault();
+      submitForm();
+    });
   } catch (error) {
     console.log(error.massage);
   }
